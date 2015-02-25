5c132e37394baffcd1be067aebae7ad232e278d1
Steven Reitsma <steven@properchaos.nl>
2015-02-25 18:48:47 +0100

    Added checkin list generator.

--- LoLTournament/Helpers/ExportHelper.cs
+++ LoLTournament/Helpers/ExportHelper.cs
@@ -10,6 +10,9 @@ namespace LoLTournament.Helpers
 {
     public class ExportHelper
     {
+        /// <summary>
+        /// Exports a list that can be used by Joris' automatic badge creator.
+        /// </summary>
         public static void ExportBadgeList()
         {
             var client = new MongoClient();
@@ -38,6 +41,38 @@ namespace LoLTournament.Helpers
             }
 
             File.WriteAllText(HttpRuntime.AppDomainAppPath + "/badge_list.csv", csv.ToString(), new UTF8Encoding(false)); // no BOM
+        }
+
+        /// <summary>
+        /// Exports a CSV file that can be used at the event checkin.
+        /// </summary>
+        public static void ExportEntryList()
+        {
+            var client = new MongoClient();
+            var server = client.GetServer();
+            var db = server.GetDatabase("CLT");
+            var col = db.GetCollection<Participant>("Participants");
+            var teamCol = db.GetCollection<Team>("Teams");
+
+            var csv = new StringBuilder();
+            csv.AppendLine("SummonerName, FullName, TeamName, StudyProgram, Captain, RU");
+
+            var validTeams = teamCol.FindAll()
+                .OrderByDescending(x => x.AmountOfRuStudents)
+                .ThenBy(x => x.Participants.Sum(y => y.RegisterTime.Ticks))
+                .Take(32)
+                .OrderBy(x => x.Name);
+
+            foreach (var p in col.FindAll().OrderBy(x => x.Team.Name))
+            {
+                if (validTeams.Any(x => x.Id == p.TeamId))
+                {
+                    var str = string.Format("{0},{1},{2},{3},{4},{5}", p.Summoner.Name, p.FullName, p.Team.Name, p.StudyProgram, p.IsCaptain, p.RuStudent);
+                    csv.AppendLine(str);
+                }
+            }
+
+            File.WriteAllText(HttpRuntime.AppDomainAppPath + "/checkin_list.csv", csv.ToString(), new UTF8Encoding(false)); // no BOM
         }
     }
 }
